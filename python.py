1e07e3d6d4e0ade67bea3d9961e9ccf127fea513
Jason Closterman <devjclosterman@gmail.com>
2023-06-03 14:58:00 -0700

    hackerRank algo

---
+++ python.py
@@ -0,0 +1,48 @@
+s = 20 + 14
+print(s)
+
+print("Hello")
+
+
+
+x = 20 + 14
+y = 15 - 14
+
+print(bool(x))
+print(bool(y))
+
+#numbers:
+num1 = 20
+num2 = 14
+
+# add two numbers
+
+sum = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+modulus = num1 % num2
+exponent = num1 ** num2
+floor_division = num1 // num2
+
+q = 20
+w = 14
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+x = 5
+#both have to be true
+print(x > 3 and x < 10)
+#return true if one statement is true
+print(x > 3 or x < 4)
+#reverse the result
+print(not(x > 3 and x < 10))
+
+#Booleans
+print(bool("Hello"))
+print(bool(15))
